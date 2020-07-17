05e593b2a87c5704425bcaeb6a7f9424c806d313
Minhaas <minhaasmohammed@gmail.com>
2020-07-18 01:54:20 +0530

    Added operations

--- basics/lists.py
+++ basics/lists.py
@@ -13,6 +13,25 @@ print("Average grade is: ",meanValue)
 seconds = [1.2323442655, 1.4534345567, 1.023458894]
 current = 1.10001399445
 #Append current to list 
-
 seconds.append(current)
 print("Appended list is: ",seconds)
+
+#Remove items from list
+seconds.remove(1.2323442655)
+print(seconds)
+
+#Accessing list
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+print(serials[2])
+print(serials[0], serials[2], serials[5])
+
+#Accessing and appending 
+workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+weekend = ["Sat", "Sun"]
+workdays.append(weekend[0])
+print("Appended list: ",workdays)
+
+#slicing
+print("Sliced list" ,workdays[1:3])
+print("First three elements (Slicing): ",workdays[:3])
+print("Last three (Negative index Slicing): ",workdays[-3:])
